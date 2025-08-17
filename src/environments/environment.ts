c00d2d5aced041fbd3ca1cf115bc3a378ae75e9a
RodrigoCG <rchuquimia94@gmail.com>
2025-08-17 18:27:49 -0400

    correcion de key del servicio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   ApiUrl: 'https://api.openweathermap.org/data/2.5',
-  ApiKey: process.env['NG_APP_API_KEY'] || ''
+  ApiKey: '__NG_APP_API_KEY__'
 };
